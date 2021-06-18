5c6e7cde7c8702c8dae88d5afbf0c16a8684004f
Josue Castillo <30334352+josuedavi@users.noreply.github.com>
2021-06-17 20:15:03 -0400

    added auth

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
   return (
     <div className="App">
-        <h1>Hello from V2</h1>
+      <header>
+        <h1>We now have Auth!</h1>
+      </header>
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
